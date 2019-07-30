c9d756591ba5e160504299c462df5858081ffff1
vigyat123 <vigyattripathi@gmail.com>
2019-07-30 19:35:50 +0530

    Change User to Status

--- src/main/java/com/assignment/ecommerceApp/controllers/UserRestController.java
+++ src/main/java/com/assignment/ecommerceApp/controllers/UserRestController.java
@@ -1,17 +1,27 @@
 package com.assignment.ecommerceApp.controllers;
 
-import com.assignment.ecommerceApp.dto.User;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.assignment.ecommerceApp.dto.Status;
+import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin()
 @RestController
 public class UserRestController {
     @GetMapping(produces = "application/json")
     @RequestMapping({ "/validateLogin" })
-    public User validateLogin() {
-        return new User("User successfully authenticated");
+    public Status validateLogin() {
+        return new Status("User successfully authenticated");
     }
+
+    /*@RequestMapping(
+            value = "/createUser",
+            method = RequestMethod.POST,
+            consumes = MediaType.ALL_VALUE,
+            produces = MediaType.ALL_VALUE
+    )
+    public @ResponseBody
+    ResponseEntity<Void> createUser(@RequestBody @Valid final Inventory inventory){
+        inventoryService.addItem(inventory);
+        return new ResponseEntity<>(HttpStatus.ACCEPTED);
+    }*/
+
 }
